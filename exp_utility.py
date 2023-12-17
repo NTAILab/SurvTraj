93928ee29ae566b88e559ba45b52532c0662d468
stasychbr <stasychbr@mail.ru>
2023-12-17 17:59:42 +0300

    Utility script update

--- exp_utility.py
+++ exp_utility.py
@@ -1,5 +1,6 @@
 import numpy as np
 import json
+from sys import argv
 
 def print_summary(filename):
     with open(filename, 'r') as file:
@@ -7,7 +8,9 @@ def print_summary(filename):
         for key, val in d.items():
             print(key)
             print(f'\tmean: {np.mean(val)}, std: {np.std(val)}')
-            
+
+DEFAULT_FILENAME = 'VetTry.json'
+
 if __name__=='__main__':
-    filename = 'VetTry.json'
+    filename = argv[1] if len(argv) > 1 else DEFAULT_FILENAME
     print_summary(filename)
